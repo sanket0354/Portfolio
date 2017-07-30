d53691c75bcdff747547eb17a952ff5f3623efc8
Sanket Patel <sanket0354@gmail.com>
2017-07-30 17:55:17 -0400

    Portfolio Version 1

---
+++ js/home.js
@@ -0,0 +1,12 @@
+
+/* Cover pic text animation using funnytext.js*/
+$(document).ready(function() {
+    $('.mySelector').funnyText({
+        speed: 400,
+        borderColor: 'white',
+        activeColor: 'white',
+        color: 'white',
+        fontSize: '4em',
+        direction: 'both'
+    });
+}); 
